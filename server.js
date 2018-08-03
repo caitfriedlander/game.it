62b57885c3f0434dee1eec344e6094299d64202b
Cait Friedlander <caitfinchart@gmail.com>
2018-08-02 17:02:45 -0700

    authentication up and running

--- server.js
+++ server.js
@@ -3,9 +3,13 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+// cors
 require('dotenv').config();
+var session = require('express-session');
+var passport = require('passport');
 
 require('./config/database');
+require('./config/passport')
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -21,6 +25,14 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(session({
+  secret: 'MLGPROGAMERSONLY',
+  resave: false,
+  saveUninitialized: true
+}));
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
