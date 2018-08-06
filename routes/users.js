b5a0ea64e1c399ea4b25a7ac9b09fa462a01362a
Cait Friedlander <caitfinchart@gmail.com>
2018-08-06 09:03:34 -0700

    removed newuser

--- routes/users.js
+++ routes/users.js
@@ -3,8 +3,6 @@ var router = express.Router();
 var usersCtrl = require('../controllers/users');
 
 router.get('/', usersCtrl.index);
-router.get('/new', usersCtrl.new);
-router.post('/', usersCtrl.create);
 router.get('/:id', usersCtrl.show);
 router.delete('/:id', usersCtrl.delete);
 router.get('/:id/beers/new', usersCtrl.newLibItem)
