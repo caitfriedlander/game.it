45c63c3fa54dd76f844a03686eb199e12e7ff46c
MilcahHalili <halilicenteno@gmail.com>
2018-08-06 10:34:02 -0700

    fix err in javascripts/app.js

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,5 +1,7 @@
 document.addEventListener("DOMContentLoaded", function() {
 
     // get our connection to the socket.io server
-    var socket = io();
+    var socket = io.connect('http://localhost:3000');
     console.log(socket);
+
+});
