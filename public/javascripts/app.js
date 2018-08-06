e1feb758cba03051810cb17b33ac4a193e0b58e5
MilcahHalili <halilicenteno@gmail.com>
2018-08-06 10:53:06 -0700

    add chat show view and update chat functionality

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,7 +1,24 @@
-document.addEventListener("DOMContentLoaded", function() {
-
-    // get our connection to the socket.io server
-    var socket = io.connect('http://localhost:3000');
-    console.log(socket);
-
-});
+document.addEventListener("DOMContentLoaded", function() { 
+    var messages = document.getElementById('messages');
+    var newMsg = document.getElementById('new-msg');
+    var userName = document.getElementById('user-name');
+    
+    var socket = io();
+    socket.on('add-message', function (data) {
+      addMessage(data);
+    });
+  
+    document.getElementById('btn-send-msg').addEventListener('click', function() {
+      socket.emit('add-message', {
+        name: userName.value,
+        msg: newMsg.value
+      });
+      newMsg.value = '';
+    });
+  
+    function addMessage(data) {
+      messages.innerHTML += ['<li><strong>', data.name, ':</strong> ', data.msg + '</li>'].join('');
+    }
+  
+  });
+  
