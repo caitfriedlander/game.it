7b5664d1bba4e037430aec53944dc41b6ee0836f
Cait Friedlander <caitfinchart@gmail.com>
2018-08-07 11:35:58 -0700

    creating games entry in DB

--- routes/games.js
+++ routes/games.js
@@ -3,9 +3,7 @@ var gamesCtrl = require('../controllers/games');
 
 router.get('/search', gamesCtrl.searchGames);
 router.get('/', gamesCtrl.index);
-router.get('/new', gamesCtrl.new);
-router.post('/', gamesCtrl.create);
-router.get('/:id', gamesCtrl.show);
+router.get('/:apiId', gamesCtrl.show);
 router.delete('/:id', gamesCtrl.delete);
 
 module.exports = router;
