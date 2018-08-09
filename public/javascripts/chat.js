22349cca18402663892b05d6749f1d5db9c42c9a
eddiejkim077 <eddiejkim077@gmail.com>
2018-08-09 16:55:49 -0700

    Add some chat styling

--- public/javascripts/chat.js
+++ public/javascripts/chat.js
@@ -5,7 +5,6 @@ socket.on('add-message', function (data) {
   var ul = document.querySelector(`#room-${data.roomId} ul`);
   ul.innerHTML += `
     <li id="chat-li">
-      <hr>
       <span class="chat-contents chat-time">${new Date(data.createdAt).toLocaleString()}</span>
       <span class="chat-contents">${data.username} &nbsp; &nbsp; ${data.message}</span>
     </li>
