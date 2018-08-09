103253f8d4d37c09ff55b50b7f082218e5715bc6
MilcahHalili <halilicenteno@gmail.com>
2018-08-09 14:39:19 -0700

    merge

--- public/javascripts/chat.js
+++ public/javascripts/chat.js
@@ -4,7 +4,7 @@ socket.on('add-message', function (data) {
   console.log(data);
   var ul = document.querySelector(`#room-${data.roomId} ul`);
   ul.innerHTML += `
-    <li>
+    <li id="chat-li">
       <hr>
       <span class="chat-contents chat-time">${new Date(data.createdAt).toLocaleString()}</span>
       <span class="chat-contents">${data.username} &nbsp; &nbsp; ${data.message}</span>
