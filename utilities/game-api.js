f27606ca745b9cb6dad25752c727795525585868
Cait Friedlander <caitfinchart@gmail.com>
2018-08-09 11:31:54 -0700

    working on search iomprovements

--- utilities/game-api.js
+++ utilities/game-api.js
@@ -8,20 +8,38 @@ module.exports = {
 };
 
 function searchByTitle(title) {
-    var url = `${apiUrl}?search=${title}&fields=*&expand=platforms`;
-    return new Promise(function(resolve, reject) {
-        request({
-            url: url,
-            headers: {
-                'user-key': process.env.IGDB_TOKEN,
-                Accept: 'application/json'
-            }
-         }, function(err, response, body) {
-            var gameData = JSON.parse(body);
-            // console.log(gameData);
-            resolve(gameData);
+    // var url = `${apiUrl}?search=${title}&fields=*&expand=platforms`;
+    if (!title) {
+        var url = `${apiUrl}?fields=*&order=popularity:desc&expand=platforms`;
+        return new Promise(function(resolve, reject) {
+            request({
+                url: url,
+                headers: {
+                    'user-key': process.env.IGDB_TOKEN,
+                    Accept: 'application/json'
+                }
+             }, function(err, response, body) {
+                var gameData = JSON.parse(body);
+                // console.log(gameData);
+                resolve(gameData);
+            });
         });
-    });
+    } else {
+        var url = `${apiUrl}?search=${title}&fields=*&order=popularity:desc&expand=platforms`;
+        return new Promise(function(resolve, reject) {
+            request({
+                url: url,
+                headers: {
+                    'user-key': process.env.IGDB_TOKEN,
+                    Accept: 'application/json'
+                }
+            }, function(err, response, body) {
+                var gameData = JSON.parse(body);
+                // console.log(gameData);
+                resolve(gameData);
+            });
+        });
+    }
 };
 
 function searchOneGame(id) {
